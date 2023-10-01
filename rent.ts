3733a355b3d5c1d539a81a6aed6763a43c0a66a5
cryptojuve <141768377+cryptojuve@users.noreply.github.com>
2023-10-01 15:12:54 +0200

    Create rent.ts

---
+++ rent.ts
@@ -0,0 +1,35 @@
+// Import
+import { WaitUntil, balanceToNumber, getBalances, getKeyringFromSeed, getRentalContractData, initializeApi, makeRentOffer } from "ternoa-js"
+
+
+async function main(seed: string,address: string) {
+  // Construct passage en mainnet
+  await initializeApi("wss://mainnet.ternoa.network");
+  const caps = await getBalances(address);
+  let free=balanceToNumber(caps.free);
+  console.log('Nombre de Caps :' + free);
+  // Do something
+        for (let i=351521;i < 351526; i++){
+        const creationBlockId = await getRentalContractData(i);
+        const keyring = await getKeyringFromSeed(seed);
+        makeRentOffer(i, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
+        }
+
+        for (let f=352900 ;f < 352905 ; f++){
+            const creationBlockId = await getRentalContractData(f);
+            const keyring = await getKeyringFromSeed(seed);
+            makeRentOffer(f, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
+        }
+
+        for (let g=359051 ;g < 359057 ; g++){
+            const creationBlockId = await getRentalContractData(g);
+            const keyring = await getKeyringFromSeed(seed);
+            makeRentOffer(g, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
+        }
+}
+
+
+const seed="wild bunker stick anxiety label forum fine measure soap best bomb monitor";
+const address="5FPDxicQroicPbkbGWxgesv29LLBhnMcJ4Pm38MUdPppcgqd";
+
+main(seed,address);
