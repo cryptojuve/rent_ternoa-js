049a4a47a1476502e4e843cb068c98a3795de024
cryptojuve <141768377+cryptojuve@users.noreply.github.com>
2023-10-17 17:51:24 +0200

    Update rent.ts
    amelioration du script, il peut etre executé plusieurs fois pour verifier si l'offre de rent est validé.
    
    Fonctionne en alphanet

--- rent.ts
+++ rent.ts
@@ -9,32 +9,32 @@ async function main(seed: string,address: string) {
   console.log('Nombre de Caps :' + free);
   var allnftId = [351521, 351522, 351523, 351524, 351525, 352900, 352901, 352902, 352903, 352904, 359051, 359052, 359053, 359054, 359055, 359056];
   try {
-      for (const nftId of allnftId) {
-           const creationBlockId = await getRentalContractData(nftId);
-          const keyring = await getKeyringFromSeed(seed);
-          const rental = await getRentalOffers(nftId);
-          if(creationBlockId!.rentee == null ){  //verifie si le rent est disponible
-          makeRentOffer(nftId, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
+    for (const nftId of allnftId) {
+        const creationBlockId = await getRentalContractData(nftId);
+        const keyring = await getKeyringFromSeed(seed);
+        const rental = await getRentalOffers(nftId);
+        if(creationBlockId!.rentee == null ){
             if (rental.find(item => item  === address) ){ //verifie si le makeRentOffer à bien fonctionné
-              console.log('MakeRentOffer done for nft node :' + nftId);
+              console.log('MakeRentOffer already done :' + nftId);
             }else {
-              console.log('Error script dont rent nft :' + nftId);
+              console.log('MakeRentOffer run for nft :' + nftId);
+              makeRentOffer(nftId, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
             }
-          }else {
-            console.log('Rent not open for nft :'+ nftId);
-          }
-      }
-  }
+        }else {
+          console.log('Rent not open for nft :'+ nftId);
+        }
+    }
+ }
   finally {
      await safeDisconnect();
      process.exit();
    }
 }
-
+// A modifier
 const seed="wild bunker stick anxiety label forum fine measure soap best bomb monitor";
 const address="5FPDxicQroicPbkbGWxgesv29LLBhnMcJ4Pm38MUdPppcgqd";
 
-//main(seed,address);
+main(seed,address);
 
 //Testnet faucet ici : https://www.ternoa.network/fr/alphanet
 
@@ -44,18 +44,18 @@ async function testnet(seed: string,address: string) {
   const caps = await getBalances(address);
   let free=balanceToNumber(caps.free);
   console.log('Nombre de Caps :' + free);
-  var test_allnftId = [54367,54368,54369,54370,54371,54372,54373,54374,54375,54376,54598,54599,55041,55042,72126,72224];
+  var test_allnftId = [82007,82008,82009,82010,82011,82012,82013,82014,82015,82016,82017,82018,82019,82020,82021,82022];
   try {
       for (const test_nftId of test_allnftId) {
           const creationBlockId = await getRentalContractData(test_nftId);
           const keyring = await getKeyringFromSeed(seed);
           const rental = await getRentalOffers(test_nftId);
           if(creationBlockId!.rentee == null ){
-          makeRentOffer(test_nftId, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
               if (rental.find(item => item  === address) ){ //verifie si le makeRentOffer à bien fonctionné
-                console.log('MakeRentOffer done for nft node :' + test_nftId);
+                console.log('MakeRentOffer already done :' + test_nftId);
               }else {
-                console.log('Error script dont rent nft :' + test_nftId);
+                console.log('MakeRentOffer run for nft :' + test_nftId);
+                makeRentOffer(test_nftId, creationBlockId!.creationBlock, keyring, WaitUntil.BlockFinalization);
               }
           }else {
             console.log('Rent not open for nft :'+ test_nftId);
@@ -68,4 +68,5 @@ async function testnet(seed: string,address: string) {
    }
       
 }
-testnet(seed,address);
+
+//testnet(seed,address);
