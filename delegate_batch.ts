82a1c7831f66713ba1687249461815cad2ab82b4
cryptojuve <141768377+cryptojuve@users.noreply.github.com>
2024-02-12 21:11:04 +0100

    Update delegate_batch.ts

--- delegate_batch.ts
+++ delegate_batch.ts
@@ -1,69 +1,61 @@
-import { BatchCompletedEvent,balanceToNumber, getBalances, safeDisconnect , BatchInterruptedEvent, WaitUntil, batchAllTxHex, batchTxHex, delegateNftTx, getKeyringFromSeed, initializeApi, submitTxBlocking } from "ternoa-js"
+import { BatchCompletedEvent,  WaitUntil,  batchTxHex,  delegateNftTx, getKeyringFromSeed, initializeApi, safeDisconnect,  submitTxBlocking } from "ternoa-js"
 
-const UnDelegateTX = async (reseau: string,seed: string,allnftId: Array<number>) => {
-  try {
-      await initializeApi(reseau);
-      const keyring = await getKeyringFromSeed(seed);
-      var allTx=new Array(0);
-      for (let i = 0; i < allnftId.length; i++) {
-          const delegueTx = await delegateNftTx(allnftId[i], undefined);
-          allTx.push(delegueTx);
-      } 
-      console.log('Nombre de TX : '+allTx.length);
-      if (allTx.length>=1){
-        const signableBatchTx = await batchTxHex(allTx);
-        const batchedEvents= await submitTxBlocking(signableBatchTx,WaitUntil.BlockInclusion,keyring);
-        const isBatchCompleted = batchedEvents.events.findEvents(BatchCompletedEvent);
-        if (isBatchCompleted.length <= 0) {
-            console.log('UnDelegate Fail');
-        } else {
-            console.log('UnDelegate Done');
+const unDelegateTX = async (seed: string,allnftId: Array<number>) => {
+    try {
+    await initializeApi("wss://mainnet.ternoa.network");     
+    const keyring = await getKeyringFromSeed(seed);
+    var allTx=new Array(0);
+    for (let i = 0; i < allnftId.length; i++) {
+          const txExtrinsic = await delegateNftTx(allnftId[i], undefined );
+          allTx.push(txExtrinsic);
+        } 
+    const signableBatchTx = await batchTxHex(allTx);
+    const batchedEvents= await submitTxBlocking(signableBatchTx,WaitUntil.BlockFinalization,keyring);
+    const isBatchCompleted = batchedEvents.events.findEvents(BatchCompletedEvent);
+    if (isBatchCompleted.length  <=0) {
+        console.log('4 - undelegate fail');
+        }else {
+            console.log('4 - UnDelegate done');
         }
-      }else{
-        console.log('Aucune operation lancée')
+
+      } finally {
+        await safeDisconnect();
+        process.exit();
       }
-  }finally {
+  }
+
+const DelegateTX = async (seed: string,recipient: string,allnftId: Array<number>) => {
+  try {
+  await initializeApi("wss://mainnet.ternoa.network");
+  const keyring = await getKeyringFromSeed(seed);
+  var allTx=new Array(0);
+  console.log('1 - Debut fonction');
+  for (let i = 0; i < allnftId.length; i++) {
+        const txExtrinsic = await delegateNftTx(allnftId[i], recipient );
+        allTx.push(txExtrinsic);
+      } 
+  console.log('2 - Après boucle sur les nft');
+  const signableBatchTx = await batchTxHex(allTx);
+  const batchedEvents= await submitTxBlocking(signableBatchTx,WaitUntil.BlockInclusion,keyring);
+  const isBatchCompleted = batchedEvents.events.findEvents(BatchCompletedEvent);
+    if (isBatchCompleted.length > 0) {
+        console.log('3 - Delegate done - start undelegate');
+        await unDelegateTX(seed,allnftId);
+    } else {
+        console.log('3 - Delegate fail');
+    }
+}finally {
     await safeDisconnect();
     process.exit();
   }
 }
 
-const DelegateTX = async (reseau:string,seed: string,recipient: string,allnftId: Array<number>) => {
-  try {
-      await initializeApi(reseau);
-      const keyring = await getKeyringFromSeed(seed);
-      var allTx=new Array(0);
-      for (let i = 0; i < allnftId.length; i++) {
-          const delegueTx = await delegateNftTx(allnftId[i], recipient);
-          allTx.push(delegueTx);
-      } 
-      console.log('Nombre de TX : '+allTx.length);
-      if (allTx.length>=1){
-        const signableBatchTx = await batchTxHex(allTx);
-        const batchedEvents= await submitTxBlocking(signableBatchTx,WaitUntil.BlockInclusion,keyring);
-        const isBatchCompleted = batchedEvents.events.findEvents(BatchCompletedEvent);
-        if (isBatchCompleted.length <= 0) {
-            console.log('Delegate Fail');
-        } else {
-            console.log('Delegate Done');
-            await UnDelegateTX(reseau,seed,allnftId);
-        }
-      }else{
-        console.log('Aucune operation lancée')
-      }
-  }finally {
-    await safeDisconnect();
-    process.exit();
-  }
-}
-
+const recipient = "5FKpteZCNEWd6rUa1RxMdzG8gir3SufzYVTKRqyoKix4RRKX"; //l'adresse du monstre
 const seed = ""; //votre seed
 const allnftId = [11111,22222,33333]; // mettre le numero des 100 nft separé par une virgule
 
-const recipient = "5FKpteZCNEWd6rUa1RxMdzG8gir3SufzYVTKRqyoKix4RRKX"; //l'adresse du monstre
-const mainnet ="wss://mainnet.ternoa.network";
-const alphanet =""; //Pour tester en alphanet remplacer à la ligne 66 la valeur mannaint par alphanet
 
-  DelegateTX(mainnet,seed,recipient,allnftId);
+
+DelegateTX(seed,recipient,allnftId);
 
 
